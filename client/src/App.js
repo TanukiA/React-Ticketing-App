426e7583296eda010e5b53554bc9c0b34741c73a
Tanuki <bukubukuu47@gmail.com>
2024-01-15 01:42:24 +0800

    Updated dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import jwt_decode from "jwt-decode";
 
 function App() {
 
   function handleCallbackResponse(response){
     console.log("Encoded JWT ID token: " + response.credential);
+    var userObject = jwt_decode(response.credential);
+    console.log(userObject);
   }
 
   useEffect(() => {
