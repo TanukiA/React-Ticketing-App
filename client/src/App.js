a2db7ef1b2d69f6e63504636fe1bb103293c98f7
Tanuki <bukubukuu47@gmail.com>
2024-01-15 06:08:24 +0800

    Added google sign up

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import jwt_decode from "jwt-decode";
+import { jwtDecode } from "jwt-decode";
 
 function App() {
 
@@ -7,9 +7,16 @@ function App() {
 
   function handleCallbackResponse(response){
     console.log("Encoded JWT ID token: " + response.credential);
-    var userObject = jwt_decode(response.credential);
+    var userObject = jwtDecode(response.credential);
     console.log(userObject);
     setUser(userObject);
+    // Hide sign in button after signed in
+    document.getElementById("signInDiv").hidden = true;
+  }
+
+  function handleSignOut(event){
+    setUser({});
+    document.getElementById("signInDiv").hidden = false;
   }
 
   useEffect(() => {
@@ -23,12 +30,23 @@ function App() {
       document.getElementById("signInDiv"),
       { theme: "outline", size: "large"}
     );
+
+    google.accounts.id.prompt();
   }, []);
 
   return(
     <div className="App">
       <div id="signInDiv"></div>
-      {}
+      {
+        Object.keys(user).length !== 0 &&
+        <button onClick={(e)=> handleSignOut(e)}>Sign out</button>
+      }
+      {user &&
+        <div>
+          <img src={user.picture} alt="userIcon"></img>
+          <h3>{user.name}</h3>
+        </div>
+      }
     </div>
   );
 }
