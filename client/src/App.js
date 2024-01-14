7a300c5f44bc0ce80be225f7b15775c33a39c940
Tanuki <bukubukuu47@gmail.com>
2024-01-15 04:56:38 +0800

    Updated dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,13 @@ import jwt_decode from "jwt-decode";
 
 function App() {
 
+  const [ user, setUser ] = useState({});
+
   function handleCallbackResponse(response){
     console.log("Encoded JWT ID token: " + response.credential);
     var userObject = jwt_decode(response.credential);
     console.log(userObject);
+    setUser(userObject);
   }
 
   useEffect(() => {
@@ -25,33 +28,9 @@ function App() {
   return(
     <div className="App">
       <div id="signInDiv"></div>
+      {}
     </div>
   );
-  /*
-  const [backendData, setBackendData] = useState([{}]);
-
-  useEffect(() => {
-    fetch("/api").then(
-      response => response.json()
-    ).then(
-      data => {
-        setBackendData(data);
-      }
-    )
-  }, []);
-
-  return (
-    <div>
-      {(typeof backendData.users === 'undefined') ? (
-        <p>Loading...</p>
-      ): (
-        backendData.users.map((user, i) => (
-          <p key={i}>{user}</p>
-        ))
-      )}
-    </div>
-  );
-  */
 }
 
 export default App;
