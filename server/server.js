6f1ddfb5dfc3af1a79d8701e914322b9695c6cfd
Tanuki <bukubukuu47@gmail.com>
2024-01-15 00:43:28 +0800

    Updated dependencies

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get("/api", (req, res) => {
-    res.json({"users": ["userOne", "userTwo", "userThree"]})
-})
+app.get('/', (req, res) => {
+    res.send('Hello, this is your Node.js server!');
+});  
 
 app.listen(5000, () => {console.log("Server started on port 5000")})
